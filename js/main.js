5dde0f73fe7fd92944a697940524cf6d08003494
Qashworth <rashworth51@gmail.com>
2022-08-01 18:12:01 -0600

    1.0

--- js/main.js
+++ js/main.js
@@ -1,40 +1,73 @@
-/*
-set an Array of 5 of the 10 team logos selected randomly
-
-duplicate that Array
-
-inject both Arrays to the section ID's
-
-first click, flip any one ID
-
-second click check if assigned team logo matches first ID
-    if(yes && all cards flipped){
-        alert (Nice! reset?)
-    } else if (yes) {
-        leave card flipped
-    } else {
-        flip cards back over
+let Match = {
+    restart: ()=>{
+      Match.flipped = [0,0,0,0,0,0,0,0,0,0,0,0]
+      Match.clear()
+      Match.randomize()
+    },
+    removeEvents:()=>{
+      document.querySelectorAll('section').forEach(x=>x.removeEventListener("click", Match.flip))
+    },
+    addEvents: ()=>{
+      document.querySelectorAll('section').forEach(x=>x.addEventListener("click", Match.flip))
+    },
+    choices: [],
+    randomArray: [],
+    randomArray2: [],
+    cardsArray: [],
+  
+    randomize: ()=>{
+      Match.cardsArray=[]
+      Match.randomArray=[]
+      Match.randomArray2=[]
+      while(Match.randomArray.length<6){
+        Match.randomArray.push(Math.floor(Math.random()*6))
+        Match.randomArray = [...new Set(Match.randomArray)]
+      }
+      while(Match.randomArray2.length<6){
+        Match.randomArray2.push(Math.floor(Math.random()*6))
+        Match.randomArray2 = [...new Set(Match.randomArray2)]
+      }
+      let offset = Math.ceil(29*Math.random())
+      Match.cardsArray = Match.randomArray.concat(Match.randomArray2)
+      Match.cardsArray.forEach((x,i)=>{document.querySelectorAll('section')[i].classList=`card${x+offset} flip`})
+      console.log(Match.cardsArray)
+    },
+    flipped: [0,0,0,0,0,0,0,0,0,0,0,0],
+    flip: (e)=>{
+      if(!Match.flipped[e.target.id]){
+      Match.removeEvents()
+        Match.choices.push(e.target.id)
+        console.log(Match.choices)
+        e.target.classList.toggle("flip")
+        Match.flipped[e.target.id] = 1
+  
+      if (Match.choices.length===2){
+        Match.check()
+      }else{
+        Match.addEvents()
+      }
     }
-
-*/
-
-const cards = document.querySelectorAll('section')
-
-//add mlb class to cards
-cards.forEach((x) => x.classList.add('mlb'))
-
-//create the first array
-let cardArray = []
-
-//Get 5 random numbers between 0-9 number
-
-// while(cardArray.length<5){
-//     let randomSort = Math.floor(Math.random() * 10);
-//     if(cardArray.indexOf(randomSort) === -1) cardArray.push(randomSort);
-// }
-//add '.card' to the random number
-cardArray = cardArray.map(i => 'card' + i)
-//make a duplicate array
-let cardArray2 = cardArray
-console.log(cardArray)
-
+    },
+    check: ()=>{
+  if(Match.cardsArray[Match.choices[0]]!==Match.cardsArray[Match.choices[1]]){
+    setTimeout(()=>{
+    document.querySelectorAll('section')[Match.choices[0]].classList.toggle('flip')
+    document.querySelectorAll('section')[Match.choices[1]].classList.toggle('flip')
+    Match.flipped[Match.choices[0]]=0
+    Match.flipped[Match.choices[1]]=0
+    Match.addEvents()
+    Match.clear()
+  },1000)
+  }else{
+  Match.clear()
+  Match.addEvents()
+  
+  }},
+  
+    clear: ()=>{
+  Match.choices = []
+  },
+  }
+  //============Adds Event Listeners===============//
+  Match.randomize()
+  Match.addEvents()
